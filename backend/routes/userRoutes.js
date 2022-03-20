be1410776edaeb56aa4681084375bc7120f4dacf
satyansh28 <satyansh.shukla.28@gmail.com>
2022-03-20 15:24:55 +0530

    changed user blacklist

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,23 +11,9 @@ router.patch("/blacklist",ensureAuth,isAdmin,async(req,res)=>
         const userid=req.query.userid;
         const change = { blackList: !req.body.blacklist };
         await user.findByIdAndUpdate(userid,change);
-        const session=await user.startSession();
-        session.startTransaction();
-        try{
-            const transaction_option={session};
-            await user.findByIdAndUpdate(userid,change,transaction_option);
-            await post.updateMany({"userid":mongoose.Types.ObjectId(userid)},change,transaction_option);
-            await reply.updateMany({"userid":mongoose.Types.ObjectId(userid)},change,transaction_option);
-            await session.commitTransaction();
-            session.endSession();
-            res.send("OK");
-        }
-        catch(e){
-            await session.abortTransaction();
-            session.endSession();
-            throw new Error();
-        }
-
+        await post.updateMany({"userid":mongoose.Types.ObjectId(userid)},change,transaction_option);
+        await reply.updateMany({"userid":mongoose.Types.ObjectId(userid)},change,transaction_option);
+        res.send("OK");
     }
     catch(e){
         res.status(400).send();
